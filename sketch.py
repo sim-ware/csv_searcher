beb6eb8a86517acaf4b1285768215b5f56ce3e92
Samir Gossain <samir.gossain@gmail.com>
2018-07-11 14:52:54 +0100

    Refactor approach

--- sketch.py
+++ sketch.py
@@ -5,7 +5,9 @@ from fuzzywuzzy import process
 # Use Pandas to read the csv into a DataFrame
 #############################################
 df = pd.read_csv('search_dataset.csv')
-
+names = df['name'].tolist()
+print(df.head())
+print(names[0:9])
 
 # Having put some examples into a .txt file, read them into a List
 ##################################################################
@@ -14,12 +16,43 @@ x = f.readlines()
 f.close()
 
 x = [line.strip('\n') for line in x]
+print(x)
+
+##############
+example = x[0]
+print(example)
 
 
-# Return the FuzzFactor for the First Query
-###########################################
-choices = df['name'].tolist()
-searchterm = x[3]
-result = process.extract(searchterm, choices, limit=10)
-print(searchterm)
-print(result)
+
+
+#TODO: Create a Dict, and List to store each one in.
+#TODO: Dict should have score//id//name//brand
+
+# mega_l=[]
+# for idx, searchterm in enumerate(x):
+#     l = []
+#     result = process.extract(searchterm, choices, limit=10)
+#     print(result)
+#     print(idx)
+#     print(searchterm)
+#     for idx,item in enumerate(result):
+#         d={}
+#         print(idx)
+#         print(item)
+#         print(item[0])
+#         yup = df.loc[df['name'] == item[0]].to_dict()
+#         print(yup)
+#         l.append(d)
+#     mega_l.append(l)
+#
+# print(mega_l)
+
+
+#   n
+# score,id,name,brand
+# score,id,name,brand
+# ...
+#
+# 2
+# 0.9242,564,asos red bag,asos
+# 0.7654,1245,reddish small bag,asos
