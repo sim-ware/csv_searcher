63231f22724d58f506e7e9693847e2a87bd95695
Samir Gossain <samir.gossain@gmail.com>
2018-07-11 17:02:25 +0100

    Adds comments

--- sketch.py
+++ sketch.py
@@ -3,23 +3,23 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 
-# Use Pandas to read the csv into a DataFrame
-#############################################
+# Use Pandas to read CSV into DataFrame, and get List of all 'name' values
+##########################################################################
 df = pd.read_csv('search_dataset.csv')
 names = df['name'].tolist()
 
 
-# Having put some examples into a .txt file, read them into a List
-##################################################################
+# Read content of Example .txt file into a List
+###############################################
 f = open('example.txt', 'r')
 x = f.readlines()
 f.close()
-
 x = [line.strip('\n') for line in x]
 
 
-# Obtaining Result
-##################
+# Obtaining Results; Using FuzzyWuzzy to return a List of each example from .txt
+# file's most similar results, and scores.
+##########################################
 result_scorelist = []
 
 for example in x:
@@ -27,42 +27,26 @@ for example in x:
     result_scorelist.append(result)
 
 
-# Formatting Result
+# Formatting Result; Loop through the List of Results and query them against the
+# Original DataFrame to return the rest of the row's values - and turn them into
+# a Dictionary, to which we add the score.
 ###################
 result_list = []
 for idx,result in enumerate(result_scorelist):
     dfquery_list = []
     for i,res in enumerate(result):
-        dfquery = df.loc[df['name'] == res[0]]
-        dfquery = dfquery.to_dict('records')[0]
-        dfquery['score'] = res[1]
-        dfquery_list.append(dfquery)
+        dfquery_dict = df.loc[df['name'] == res[0]].to_dict('records')[0]
+        dfquery_dict['score'] = res[1]
+        dfquery_list.append(dfquery_dict)
     result_list.append(dfquery_list)
-# print(result_list)
-# print(len(result_list))
 
-# Render It
-###########
+
+# Render It; The results are returned as a List of List of Dictionaries, which is
+# jarring to read. Here we prettify it slightly.
+################################################
 for idx,result in enumerate(result_list):
     print('----------------------------')
     print(str(idx) + ')SEARCHTERM:' + str(x[idx]))
-    # print()
     print('----------------------------')
-    # print(result[idx])
-    # print('----------------------------')
     for idx,item in enumerate(result):
-        # print(idx)
-        # print('----------------------------')
         print('|Score:' + str(item['score']) + '|ID:' + str(item['id']) + '|Name:' + str(item['name']) + '|Brand:' + str(item['brand']))
-
-
-
-
-#   n
-# score,id,name,brand
-# score,id,name,brand
-# ...
-#
-# 2
-# 0.9242,564,asos red bag,asos
-# 0.7654,1245,reddish small bag,asos
